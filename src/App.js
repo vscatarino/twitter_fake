a3762100cb46444ed3257aec1ce3870fe93bc343
vinicius <vscatarino@github.com>
2018-12-04 01:16:03 -0200

    first elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +6,19 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div className="App-menu">
+          post
+        </div>
+
+        <div className="App-moldura">
+            <img className="App-foto-perfil" src="https://i.pinimg.com/originals/ae/19/04/ae1904b8ec612779a45d5d2b091140ea.jpg" alt="logo" />
+        </div>
+
+        <div className="App-Dados">
+          Dados
+        </div>
+
       </div>
     );
   }
